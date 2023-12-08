b1aae1763da0e37480e0d94af6c011eb369a0e1f
ZeroWV <fazenotzero@gmail.com>
2023-12-07 19:34:18 -0600

    java

---
+++ src/main/java/com/lab4tecnicas/lab4tecnicas/Lab4tecnicasApplication.java
@@ -0,0 +1,13 @@
+package com.lab4tecnicas.lab4tecnicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4tecnicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4tecnicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab4tecnicas/lab4tecnicas/Lab4tecnicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab4tecnicas.lab4tecnicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4tecnicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
